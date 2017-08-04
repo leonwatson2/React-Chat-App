e584e0ef4ba04a29863b23320ec5b4e98257f4d6
vlw0052 <leon@vlw2.com>
2017-08-03 22:39:21 -0500

    Added Name to messages & updated styles

--- src/components/messaging/Messages.js
+++ src/components/messaging/Messages.js
@@ -37,7 +37,10 @@ export default class Messages extends Component {
 							return( 
 							<div key={mes.id} className={`message-container ${mes.sender === user.name && 'right'}`}>
 								<div className="time">{mes.time ? this.getTime(mes.time) : null}</div>
-								<div className="message">{mes.message}</div>
+								<div className="data">
+									<div className="message">{mes.message}</div>
+									<div className="name">{mes.sender}</div>
+								</div>
 							</div>)
 						})
 						
