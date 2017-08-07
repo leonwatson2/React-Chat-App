5e22448371f9551faad5bc3900b027f318aa262a
vlw0052 <leon@vlw2.com>
2017-08-07 11:04:07 -0500

    AddMessageToChat update

--- src/components/chat/ChatContainer.js
+++ src/components/chat/ChatContainer.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import ChatThread from './ChatThread'
 import SideBar from './SideBar'
 
-import { Chat, Message, User } from '../../Classes'
+import { User } from '../../Classes'
 import Messages from '../messaging/Messages'
 import MessageInput from '../messaging/MessageInput'
 import ChatHeading from './ChatHeading'
@@ -53,13 +53,12 @@ export default class ChatContainer extends Component {
 	addMessageToChat(chatId){
 		return message =>{
 			const { chats } = this.state
-			chats.map((chat) => {
+			let newChats = chats.map((chat) => {
 				if(chat.id === chatId)
 					chat.messages.push(message)
-					
+				return chat;
 			})
-			console.log(chats);
-			this.setState({chats})
+			this.setState({chats:newChats})
 		}
 	}
 	/*
@@ -68,11 +67,9 @@ export default class ChatContainer extends Component {
 	*	@param message {string} The message to be added to the chat.
 	*/
 	sendMessage(chatId, message){
-		const { chats } = this.state 
+ 
 		const { socket } = this.props
 
-		const sender = this.props.user.name;
-		
 		socket.emit(MESSAGE_SENT, {chatId, message})
 		
 	}
@@ -83,8 +80,8 @@ export default class ChatContainer extends Component {
 	*	typing {boolean} If the user is typing still or not.
 	*/
 	sendTyping(chatId, isTyping){
-		const { chats } = this.state 
-		const { user } = this.props;
+		// const { chats } = this.state 
+		// const { user } = this.props;
 		
 		// const newChats = chats.map((chat)=>{
 		// 	if(chat.id === chatId){
@@ -158,294 +155,8 @@ export default class ChatContainer extends Component {
 		);
 	}
 }
-const fakeMessages = [
-		new Message({ message:"Hey Michael", sender:"Bessie McCoy"}),
-		new Message({ message:"Hey Amy", sender:"Michael Angelo"}),
-		new Message({ message:"Last Message", sender:"Bessie McCoy"})
-	]
 
-const massChat = new Chat({name:"Community Chat", messages:fakeMessages})
 
-const randomChats = [
-	massChat,
-	{
-		id:123,
-		messages:[{
-			id:11,
-			message:"Hey Michael",
-			sender:"Herbert Furgeson",
-			time:new Date("April 7, 1993 14:30"),
-			read:true
-
-		},{
-			id:1342,
-			message:"Hey Herbert",
-			sender:"Michael Angelo",
-			time:new Date("April 7, 1993 14:45"),
-			read:false
-
-		},
-		{
-			id:132,
-			message:"What's Up",
-			sender:"Herbert Furgeson",
-			time:new Date("April 7, 1993 14:50"),
-			read:false
-
-		}],
-		users:[{
-			id:23,
-			name:"Herbert Furgeson"
-		},{
-			id:24,
-			name:"Michael Angelo"
-		}],
-		typingUsers:[],
-		name:"Herbert Furgeson",
-	},
-
-	{
-		id:125,
-		messages:[{
-			id:1,
-			message:"Hey Michael",
-			sender:"Amy Daniel",
-			time:new Date("April 7, 1993 14:30"),
-			read:true
-
-		},{
-			id:1342,
-			message:"Hey Amy",
-			sender:"Michael Angelo",
-			time:new Date("April 7, 1993 14:45"),
-			read:true
-		},
-		{
-			id:13,
-			message:"What's Up",
-			sender:"Amy Daniel",
-			time:new Date("April 7, 1993 14:50"),
-			read:true
-
-		}],
-		users:[{
-			id:213,
-			name:"Amy Daniel"
-		},{
-			id:24,
-			name:"Michael Angelo"
-		}],
-		typingUsers:[],
-		name:"Amy Daniel"
-	},
-
-	{
-		id:Math.ceil(Math.random()*450054),
-		messages:[{
-			id:1,
-			message:"Hey Michael",
-			sender:"Bessie McCoy",
-			time:new Date("April 7, 1993 14:30"),
-			read:true
-
-		},{
-			id:1342,
-			message:"Hey Amy",
-			sender:"Michael Angelo",
-			time:new Date("April 7, 1993 14:45"),
-			read:false
-		},
-		{
-			id:1342,
-			message:"What's Up",
-			sender:"Bessie McCoy",
-			time:new Date("April 7, 1993 14:50"),
-			read:false
-
-		}],
-		users:[{
-			id:213,
-			name:"Bessie McCoy"
-		},{
-			id:24,
-			name:"Michael Angelo"
-		}],
-		typingUsers:[],
-		name:"Bessie McCoy"
-	},
-
-	{
-		id:Math.ceil(Math.random()*4545898),
-		messages:[{
-			id:1,
-			message:"Hey Michael",
-			sender:"Daniel Barnett",
-			time:new Date("April 7, 1993 14:30"),
-			read:true
-
-		},{
-			id:1342,
-			message:"Hey Amy",
-			sender:"Michael Angelo",
-			time:new Date("April 7, 1993 14:45"),
-			read:false
-		},
-		{
-			id:1342,
-			message:"What's Up",
-			sender:"Daniel Barnett",
-			time:new Date("April 7, 1993 14:50"),
-			read:false
-
-		}],
-		users:[{
-			id:213,
-			name:"Daniel Barnett"
-		},{
-			id:24,
-			name:"Michael Angelo"
-		}],
-		typingUsers:[],
-		name:"Daniel Barnett"
-	},
-	{
-
-			id:Math.ceil(Math.random()*45),
-			messages:[{
-				id:1,
-				message:"Hey Michael",
-				sender:"Jim Smith",
-				time:new Date("April 7, 1993 14:30"),
-				read:true
-	
-			},{
-				id:1342,
-				message:"Hey Jim",
-				sender:"Michael Angelo",
-				time:new Date("April 7, 1993 14:45"),
-				read:true
-			},
-			{
-				id:1342,
-				message:"What's Up",
-				sender:"Jim Smith",
-				time:new Date("April 7, 1993 14:50"),
-				read:true
-	
-			}],
-			users:[{
-				id:213,
-				name:"Jim Smith"
-			},{
-				id:24,
-				name:"Michael Angelo"
-			}],
-			typingUsers:[],
-			name:"Jim Smith",
-	},{
-				id:Math.ceil(Math.random()*45),
-				messages:[{
-					id:1,
-					message:"Hey Michael",
-					sender:"Myrtle West",
-					time:new Date("April 7, 1993 14:30"),
-					read:true
-		
-				},{
-					id:1342,
-					message:"Hey Amy",
-					sender:"Michael Angelo",
-					time:new Date("April 7, 1993 14:45"),
-					read:true
-				},
-				{
-					id:1342,
-					message:"What's Up",
-					sender:"Myrtle West",
-					time:new Date("April 7, 1993 14:50"),
-					read:true
-		
-				}],
-				users:[{
-					id:213,
-					name:"Myrtle West"
-				},{
-					id:24,
-					name:"Michael Angelo"
-				}],
-				typingUsers:[],
-				name:"Myrtle West"
-	},
-	{
-				id:Math.ceil(Math.random()*45),
-				messages:[{
-					id:1,
-					message:"Hey Michael",
-					sender:"Emma Koskela",
-					time:new Date("April 7, 1993 14:30"),
-					read:true
-		
-				},{
-					id:1342,
-					message:"Hey Amy",
-					sender:"Michael Angelo",
-					time:new Date("April 7, 1993 14:45"),
-					read:true
-				},
-				{
-					id:1342,
-					message:"What's Up",
-					sender:"Emma Koskela",
-					time:new Date("April 7, 1993 14:50"),
-					read:true
-		
-				}],
-				users:[{
-					id:213,
-					name:"Emma Koskela"
-				},{
-					id:24,
-					name:"Michael Angelo"
-				}],
-				typingUsers:[],
-				name:"Emma Koskela"
-	},
-	{
-			id:Math.ceil(Math.random()*45),
-			messages:[{
-				id:1,
-				message:"Hey Michael",
-				sender:"Lauri Jarvela",
-				time:new Date("April 7, 1993 14:30"),
-				read:true
-	
-			},{
-				id:1342,
-				message:"Hey Amy",
-				sender:"Michael Angelo",
-				time:new Date("April 7, 1993 14:45"),
-				read:false
-			},
-			{
-				id:1342,
-				message:"What's Up",
-				sender:"Lauri Jarvela",
-				time:new Date("April 7, 1993 14:50"),
-				read:false
-	
-			}],
-			users:[{
-				id:213,
-				name:"Lauri Jarvela"
-			},{
-				id:24,
-				name:"Michael Angelo"
-			}],
-			typingUsers:[],
-			name:"Lauri Jarvela"
-		}
-
-]
 
 
 ChatContainer.propTypes = {
