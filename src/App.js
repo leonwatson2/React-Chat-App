0f47f36c15a185d1c59a8c72b23a42059c9613f6
vlw0052 <leon@vlw2.com>
2017-08-08 11:30:18 -0500

    Importing css  styles in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout'
+import './styles/index.css'
 
 class App extends Component {
   
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css'
-
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
