6a1f80905c4b68fc8326a7dd4305edd03c44e46f
Leon Watson <code@vlw2.com>
2023-04-15 18:52:56 -0500

    updated server settings

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,27 @@
-var app = require('http').createServer()
-var io = module.exports.io = require('socket.io')(app);
+const express = require(
+  'express'
+)
+const app = express()
+const socketio = require("socket.io");
 
 const PORT = process.env.PORT || 3231
+
+const server = require('http').createServer(app)
+const io = module.exports.io = socketio(server, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
 const SocketManager = require('./SocketManager')
- 
+
+
+
+app.get('/', function (req, res) {
+  res.send('ok');
+})
 io.on('connection', SocketManager);
 
-
-app.listen(PORT, function(){
+app.listen(PORT, function () {
   console.log('listening on *:' + PORT);
 });
 
